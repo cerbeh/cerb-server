41355bc0ec59cc8dda876a6d163a7b55538b6029
Martin Allgood <martintallgood@gmail.com>
2022-05-12 18:27:49 +0100

    Added virtual host for ui project

--- server/router.js
+++ server/router.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 const express = require('express');
+const createVirtualHost = require('./vhost');
 
 const getAppDirectory = () => path.join(__dirname, '../apps');
 
@@ -21,12 +22,23 @@ const createRoutes = () => {
 const init = server => {
 
   server.get('*', function (req, res, next) {
-    console.log('Request was made to: ' + req.originalUrl);
+    const domain = req.headers.host;
+    const path = req.originalUrl;
+    console.log(`Request was made to: ${domain}${path}`);
     return next();
   });
 
   const appRoutes = createRoutes();
   server.use('/api', appRoutes);
+  const vhost = createVirtualHost('colour-clicker.*', `${process.cwd()}/ui/WDI-Project-One`);
+
+  server.use(vhost);
+
+  server.use('*', (req, res, next) => {
+    console.log('no route found. Stupid.');
+    next()
+  })
+
 };
 
 module.exports = { init };
